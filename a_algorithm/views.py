2780e6db9656ed93e1be96c126b05474562da873
Nicola <nqola.ibrahim@lit-co.net>
2023-01-03 09:56:18 +0200

    Apply A* start algorithm with initial and goal state

--- a_algorithm/views.py
+++ a_algorithm/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from . import searching
 
-# Create your views here.
+@require_http_methods(['GET', 'POST'])
+def display_game(request):
+    context = {}
+    # if (request.method == 'POST'):
+    #     print(request.POST)
 
+    if (request.method == 'GET'):
+        
+        # start state 
+        start = [8, 2, 3, 6, 1, 4, 7, 0, 5]
+        
+        # target state       
+        target = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+        
+        
 
-def index(request):
-
-    context = {
-
-        "content": {
-            "array_1": [
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 0]
-            ],
-            "array_2": [
-                [1, 3, 4],
-                [6, 0, 2],
-                [7, 8, 5]
-            ],
-
+        best_path = searching.a_start(start, target)
+        context = {
+            "content": best_path
         }
-    }
 
     return render(request=request, template_name='display.html', context=context)
